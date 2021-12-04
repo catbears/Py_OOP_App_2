4b7839c37537f50e8ad1148abc957a9aa9d3f90f
catbears <71704592+catbears@users.noreply.github.com>
2021-12-04 17:09:37 +0100

    Added auto-open pdf viewer, also finished format of pdf

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import webbrowser
 from fpdf import FPDF
 
 class Bill:
@@ -37,23 +38,37 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
+        
+        flatmate1_pay: str = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay: str = str(round(flatmate2.pays(bill, flatmate1), 2))
 
         pdf = FPDF(orientation='P', unit="pt", format="A4")
         pdf.add_page()
 
+        # Add Icon
+        pdf.image(name="./files/house.png", w=30, h=30)
+
         # Insert title
         pdf.set_font(family="Times", size=24, style="B")
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
 
         # Insert Period label and value
-        pdf.cell(w=100, h=40, txt="Period:", border=1)
-        pdf.cell(w=160, h=40, txt=bill.period, border=1, ln=1)
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=160, h=40, txt=bill.period, border=0, ln=1)
 
         # Insert name and due amount of the first flatmate
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=160, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1)
+        pdf.set_font(family="Times", size=14)
+        pdf.cell(w=100, h=20, txt=flatmate1.name, border=0)
+        pdf.cell(w=160, h=20, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=20, txt=flatmate2.name, border=0)
+        pdf.cell(w=160, h=20, txt=flatmate2_pay, border=0, ln=1)
 
         pdf.output(self.filename)
+
+        webbrowser.open_new(self.filename)
 
 the_bill = Bill(amount = 120, period = "April 2021")
 john = Flatmate(name="John", days_in_house=20)
